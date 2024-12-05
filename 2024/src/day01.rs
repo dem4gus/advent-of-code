f97737829ea992a51ec9e52e7c39340f49eca367
Jason Healy <jason@healy.app>
2024-12-04 20:59:19 -0500

    move code into module

---
+++ 2024/src/day01.rs
@@ -0,0 +1,81 @@
+use std::collections::HashMap;
+use std::iter::zip;
+
+pub fn calculate_distance(input: String) -> u64 {
+    let mut first: Vec<i64> = vec![];
+    let mut second: Vec<i64> = vec![];
+
+    for line in input.lines() {
+        let nums: Vec<_> = line
+            .split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect();
+        first.push(nums[0]);
+        second.push(nums[1]);
+    }
+
+    first.sort();
+    second.sort();
+
+    zip(first, second).map(|(x, y)| (x - y).abs() as u64).sum()
+}
+
+pub fn calculate_similarity(input: String) -> u64 {
+    let mut first: Vec<String> = vec![];
+    let mut second: HashMap<String, u64> = HashMap::new();
+
+    for line in input.lines() {
+        let nums: Vec<_> = line.split_whitespace().collect();
+        first.push(nums[0].to_string());
+        let key = nums[1].to_string();
+        if let Some(val) = second.insert(key.clone(), 1) {
+            second.insert(key, val + 1);
+        };
+    }
+
+    first
+        .iter()
+        .map(|x| {
+            return if let Some(&val) = second.get(x) {
+                x.parse::<u64>().unwrap() * val
+            } else {
+                0u64
+            };
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn difference_passes() {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3"
+            .to_string();
+        let want = 11;
+        let result = calculate_distance(input);
+
+        assert_eq!(result, want)
+    }
+
+    #[test]
+    fn similarity_passes() {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3"
+            .to_string();
+        let want = 31;
+        let result = calculate_similarity(input);
+
+        assert_eq!(result, want)
+    }
+}
--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,10 +1,13 @@
+use advent_of_code_2024::day01::{calculate_distance, calculate_similarity};
 use std::fs;
-use advent_of_code_2024::{calculate_distance, calculate_similarity};
 
 fn main() {
-    let day_one_path = "inputs/day1.txt";
+    let day_one_path = "inputs/day01.txt";
     let (part_one, part_two) = match fs::read_to_string(day_one_path) {
-        Ok(input) => {(calculate_distance(input.clone()), calculate_similarity(input))},
+        Ok(input) => (
+            calculate_distance(input.clone()),
+            calculate_similarity(input),
+        ),
         Err(error) => panic!("There was a problem calculating the answer: {}", error),
     };
 
